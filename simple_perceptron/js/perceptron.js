2611124523ae2cf93a7aa0600adf03cd7bd69e51
Angelo Bianco <a.bianco84@gmail.com>
2018-07-07 17:21:50 +0200

    Simple perceptron completed

--- simple_perceptron/js/perceptron.js
+++ simple_perceptron/js/perceptron.js
@@ -1,14 +1,54 @@
 class Perceptron {
-    constructror(renderer, inputs) {
+    constructor(renderer, inputs, learning_rate = 0.0001) {
         this.renderer = renderer;
 
-        this.inputs  = inputs + 1;
+        this.learning_rate = learning_rate;
         this.weights = [];
 
+
         const p5 =  this.renderer;
-        for(let i=0; i < this.inputs; i++) {
+        for(let i=0; i < inputs + 1; i++) {
             const weigth = p5.random(-1,1);
+            // console.log(weigth)
             this.weights.push(weigth);
         }
+
+    }
+
+    activate(sum) {
+       return ( sum >= 0 ? 1 : -1);
+    }
+
+    feedForward(inputs) {
+        const inp = [1, ...inputs];
+        let sum = 0;
+        for(let i = 0; i < this.weights.length; i++) {
+            sum += inp[i] * this.weights[i];
+        }
+
+        return this.activate(sum);
+    }
+
+    train(inputs, desidered) {
+        const guess = this.feedForward(inputs);
+        const err = desidered - guess;
+        // console.log('error: ' + err)
+        const inp = [1, ...inputs];
+        for(let i = 0; i<this.weights.length; i++) {
+            this.weights[i] += err * inp[i] * this.learning_rate;
+        }
+
+        const m = -(this.weights[1]/this.weights[2]);
+        const b = -(this.weights[0]/this.weights[2]);
+        // console.log(`y = ${m}x + ${b}`);
+    }
+
+    getY(x) {
+        const m = -(this.weights[1]/this.weights[2]);
+        const b = -(this.weights[0]/this.weights[2]);
+
+        return (m*x) + b; 
     }
 };
+
+export default Perceptron;
--- simple_perceptron/js/sketch.js
+++ simple_perceptron/js/sketch.js
@@ -1,17 +1,65 @@
 import p5 from 'p5';
 import 'p5/lib/addons/p5.dom';
+import Perceptron from './perceptron.js';
 
 const sketch = p5 => {
   let canvas;
 
+  let brain;
+  let points = [];
+  let count;
+
   p5.setup = () => {
-    canvas = p5.createCanvas(700, 600);
+    canvas = p5.createCanvas(700, 700);
     canvas.style('border', 'solid 1px');
     p5.background(p5.color(251,232,211));
+
+    
+    brain = new Perceptron(p5, 2);
+
+    for(let i = 0; i < 2000; i++){
+      const point = {
+        x: p5.random(0, p5.width),
+        y: p5.random(0, p5.height)
+      }
+      points.push(point);
+    }
+
+    count = 0;
   };
 
   p5.draw = () => {
     p5.background(255);
+
+    //Draw desidered line
+    p5.stroke(0);
+    p5.line(0,0, p5.width, p5.height);
+
+
+    //Train the perceptron a point at a time
+    const point = points[count];
+    const desiderd = (point.x > point.y) ? 1 : -1
+    brain.train([point.x, point.y], desiderd);
+    // console.log(brain.weights)
+    count = (count + 1)%points.length;
+
+    //Draw the perceptron line
+    p5.push();
+    p5.stroke(p5.color(0,255,0));
+    p5.strokeWeight(3);
+    p5.line(0, brain.getY(0), p5.width, brain.getY(p5.width));
+    p5.pop();
+
+    //Draw points
+    for(let i = 0; i < count; i++) {
+      const point = points[i];
+      p5.stroke(0)
+      // const color = (point.x > point.y) ? p5.color(255, 0, 0) : p5.color(0, 0, 255);
+      const color = brain.feedForward([point.x, point.y]) > 0 ? p5.color(255, 0, 0) : p5.color(0, 0, 255);
+      // console.log(color)
+      p5.fill(color);
+      p5.ellipse(point.x, point.y, 15,15);
+    }
 
   };
 };
